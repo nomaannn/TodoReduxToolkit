7d46948f540fb77be4c0163262122ba940eceed1
Nomaan Ahmad <94966523+nomaannn@users.noreply.github.com>
2024-02-06 20:38:56 +0530

    Add files via upload

---
+++ src/app/store.js
@@ -0,0 +1,8 @@
+// configure store
+
+import { configureStore } from '@reduxjs/toolkit'
+import todoReducer from '../features/todo/todoSlice'
+
+export const store = configureStore({
+    reducer: todoReducer
+})
